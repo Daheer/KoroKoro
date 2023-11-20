4ced85640b74dd1ef38b29e1aeda64a8473b3ef3
Ibrahim Dahiru Dahiru <34832399+Daheer@users.noreply.github.com>
2023-11-20 23:10:21 +0100

    Update logging.py

--- KoroKoro/logging.py
+++ KoroKoro/logging.py
@@ -6,7 +6,7 @@ from datetime import datetime
 logging_str = "[%(asctime)s: %(levelname)s: %(lineno)d:] %(message)s: --> %(pathname)s:"
 logger_file_name = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"	
 
-log_dir = os.path.join(os.getcwd(), "logs", logger_file_name)
+log_dir = os.path.join(os.getcwd(), "logs")
 log_filepath = os.path.join(log_dir, logger_file_name)
 os.makedirs(log_dir, exist_ok=True)
 
