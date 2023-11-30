77f88d8dc5953b3477fba758128127154d72bbf1
Ibrahim Dahiru Dahiru <34832399+Daheer@users.noreply.github.com>
2023-11-30 04:07:45 +0100

    Update model_trainer.py

--- KoroKoro/components/model_trainer.py
+++ KoroKoro/components/model_trainer.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import subprocess
 
 from KoroKoro.utils import bin_colors
 from KoroKoro.logging import logger
@@ -14,7 +15,8 @@ class ModelTrainer:
   def train_model(self) -> None:
     logger.info(f"{bin_colors.INFO}Running model for {self.config.unique_id}{bin_colors.ENDC}")
     try:
-      os.system(f"python3 instant-ngp/scripts/run.py --scene {self.config.colmap_output} --n_steps 5000 --save_mesh {self.config.obj_output}")
+      # os.system(f"python3 instant-ngp/scripts/run.py --scene {self.config.colmap_output} --n_steps 5000 --save_mesh {self.config.obj_output}")
+      subprocess.run(f"python3 instant-ngp/scripts/run.py --scene {self.config.colmap_output} --n_steps 5000 --save_mesh {self.config.obj_output}", check = True, shell = True)
       logger.info(f"{bin_colors.SUCCESS}Model trained successfully!{bin_colors.ENDC}")
       logger.info(f"{bin_colors.SUCCESS}Model successfully saved at {self.config.obj_output}{bin_colors.ENDC}")
     except Exception as e:
