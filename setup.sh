1a75f211b327ebdd9cb853ab9cfa81c1f15da4f9
Ibrahim Dahiru Dahiru <34832399+Daheer@users.noreply.github.com>
2025-01-16 05:35:40 +0000

    mod: removed building dependencies

--- setup.sh
+++ setup.sh
@@ -12,11 +12,6 @@ conda activate korokoro
 conda install -c "nvidia/label/cuda-11.8.0" cuda-toolkit -y
 conda install -c conda-forge colmap==3.8 -y
 
-# Install system dependencies
-sudo apt update
-sudo apt install build-essential git ffmpeg python3-dev python3-pip libopenexr-dev libxi-dev libglfw3-dev libglew-dev libomp-dev libxinerama-dev libxcursor-dev
-pip install --upgrade cmake
-
 # Install additional requirements
 pip install -r requirements.txt
 gdown "https://drive.google.com/u/1/uc?id=1-7x7qQfB7bIw2zV4Lr6-yhvMpjXC84Q5&confirm=t" 
