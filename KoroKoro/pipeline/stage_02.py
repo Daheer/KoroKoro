ccced5a27f225175c962b7bcf29bddab0a7981d2
Ibrahim Dahiru Dahiru <34832399+Daheer@users.noreply.github.com>
2025-01-21 10:53:39 +0000

    refactor: improve code formatting and error handling in stage 02 pipeline

--- KoroKoro/pipeline/stage_02.py
+++ KoroKoro/pipeline/stage_02.py
@@ -9,6 +9,7 @@ from KoroKoro.utils.constants import CONFIG_FILE_PATH, GROUNDING_DINO_PATH
 from KoroKoro.logger import logger
 
 import sys
+
 sys.path.append(GROUNDING_DINO_PATH)
 
 from KoroKoro.components.data_transformation import DataTransformation
@@ -19,24 +20,31 @@ dotenv.load_dotenv()
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
-config = read_config(CONFIG_FILE_PATH) 
+config = read_config(CONFIG_FILE_PATH)
 
 try:
-  logger.info(f"{bin_colors.INFO}Starting reconstruction pipeline{bin_colors.ENDC}")
-  supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
-  DataTransformation().transform_data()
-  ModelTrainer().train_model()
-  # os.system("python3 KoroKoro/components/post_processing.py")
-  subprocess.run("python3 KoroKoro/components/post_processing.py", check = True, shell = True)
-  logger.info(f"{bin_colors.SUCCESS}Stage 02 of reconstruction pipeline executed successfully!{bin_colors.ENDC}")
+    logger.info(f"{bin_colors.INFO}Starting reconstruction pipeline{bin_colors.ENDC}")
+    supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+    DataTransformation().transform_data()
+    ModelTrainer().train_model()
+    # os.system("python3 KoroKoro/components/post_processing.py")
+    subprocess.run(
+        "python3 KoroKoro/components/post_processing.py", check=True, shell=True
+    )
+    logger.info(
+        f"{bin_colors.SUCCESS}Stage 02 of reconstruction pipeline executed successfully!{bin_colors.ENDC}"
+    )
 except Exception as e:
-  supabase.table('products').update({'status': 'FAILED'}).eq('unique_id', config.unique_id).execute()
-  logger.error(f"{bin_colors.ERROR}Error while running pipeline: {e}{bin_colors.ENDC}")
-  raise e
+    supabase.table("products").update({"status": "FAILED"}).eq(
+        "unique_id", config.unique_id
+    ).execute()
+    logger.error(
+        f"{bin_colors.ERROR}Error while running pipeline: {e}{bin_colors.ENDC}"
+    )
+    raise e
 finally:
-  if not os.path.exists(f"artifacts/{config.unique_id}/part1.txt"):
-    supabase.table('products').update({'status': 'FAILED'}).eq('unique_id', config.unique_id).execute()
-    logger.error(f"{bin_colors.ERROR}Error while running pipeline{bin_colors.ENDC}")
-
-
-  
+    if not os.path.exists(f"artifacts/{config.unique_id}/splat.ply"):
+        supabase.table("products").update({"status": "FAILED"}).eq(
+            "unique_id", config.unique_id
+        ).execute()
+        logger.error(f"{bin_colors.ERROR}Error while running pipeline{bin_colors.ENDC}")
